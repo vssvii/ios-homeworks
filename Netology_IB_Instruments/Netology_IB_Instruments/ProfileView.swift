f8ef5fa09e235cd93c3e582e7befb84f7f294546
Ibragim Assaibuldayev <Ibragimktl95@gmail.com>
2021-10-31 22:07:41 +0600

    Import Netology_IB_Instruments content

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Ibragim Assaibuldayev on 18.10.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myBirth: UILabel!
+    @IBOutlet weak var myText: UITextView!
+    @IBOutlet weak var myLocation: UILabel!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Ibragim Assaibuldayev on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

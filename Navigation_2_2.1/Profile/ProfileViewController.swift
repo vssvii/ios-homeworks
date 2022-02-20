2803277f7575a6867f8cac847b3dccbe2f63cc9e
Ibragim Assaibuldayev <Ibragimktl95@gmail.com>
2022-02-21 02:11:50 +0600

    Updated_hw2.1_v2

--- Navigation_2_2.1/Profile/ProfileViewController.swift
+++ Navigation_2_2.1/Profile/ProfileViewController.swift
@@ -7,61 +7,33 @@
 
 import UIKit
 
+
+
 class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .lightGray
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
+        view.backgroundColor = .white
+        
+        let allSidesPadding = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+        let yFrame = allSidesPadding.top * 6
+        let widthFrame = view.frame.width
+        let heightFrame = view.frame.height
+        
         let profileHeader = ProfileHeaderView()
         profileHeader.frame = CGRect(
             x: 0,
-            y: 0,
-            width: 428.0,
-            height: 926.0
+            y: yFrame,
+            width: widthFrame,
+            height: heightFrame
         )
+        profileHeader.backgroundColor = .lightGray
         
-         view.addSubview(profileHeader)
-        
-        let button = UIButton(frame: CGRect(x: 16, y: 161, width: 296, height: 50))
-        button.backgroundColor = .blue
-        button.layer.cornerRadius = 4.0
-        button.setTitle("Показать статус", for: .normal)
-        button.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
-        button.layer.shadowRadius = 4.0
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.7
-        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        profileHeader.addSubview(button)
-        
-        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-            textField.center = CGPoint(x: 220, y: 140)
-            textField.textAlignment = .center
-            textField.font = .boldSystemFont(ofSize: 15)
-            textField.textColor = .black
-            textField.backgroundColor = .white
-            textField.layer.borderColor = UIColor.black.cgColor
-            textField.layer.borderWidth = 1
-            textField.layer.cornerRadius = 12
-            textField.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
-        profileHeader.addSubview(textField)
+        view.addSubview(profileHeader)
     }
-    
-        @objc func buttonPressed() {
-            
-            let alertController = UIAlertController(title: "Статус", message: statusText, preferredStyle: .alert)
-            let statusAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(statusAction)
-            present(alertController, animated: true, completion: nil)
-        }
-    
-    @objc func statusTextChanged(_ textField: UITextField) {
-        statusText = textField.text ?? "Error"
-    }
-    private var statusText:String = ""
 }
